03948e8c36c977c79c51bb29f0e9c7088f05c6d4
Bizhan Amouzad <b_amouzad1988@yahoo.com>
2023-04-29 02:31:23 +0430

    re,ove ssr page and add ssr code to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Counter from "@/components/counter/counter";
 import PostList from "@/components/posts/postList";
+import { incNum } from "@/store/slices/counterSlice";
+import { fetchPosts } from "@/store/slices/postsSlice";
+import { wrapper } from "./../store/store";
 
 export default function Home() {
   return (
@@ -9,3 +12,19 @@ export default function Home() {
     </>
   );
 }
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    store.dispatch(incNum(4));
+    const state = store.getState();
+
+    const data = await store.dispatch(fetchPosts());
+    const posts = JSON.stringify(data.payload);
+
+    return {
+      props: {
+        count: state.counter.value,
+        posts,
+      },
+    };
+  }
+);
